8fbc4d43b783d94a81af49ef7a371b050968bde0
Aditya16-K <golukumar4288513@gmail.com>
2025-08-31 19:03:47 +0545

    changes in backend

--- server/controllers/creditController.js
+++ server/controllers/creditController.js
@@ -88,7 +88,7 @@ export const purchasePlans = async (req, res) => {
       success_url: `${origin}/loading`,
       cancel_url: `${origin}`,
       metadata: { transaction: transaction._id.toString(), appId: 'chatbot' },
-      expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
+      expires_at: Math.floor(Date.now() / 1000) + 60 * 60,
     });
 
     res.json({ success: true, url: session.url });
